7f500e79a708ad7a304f064a6197091ee4af5c06
Юлия <julia4eck@gmail.com>
2019-10-02 20:36:07 +0300

    Third lesson all except hard tacks fixing an error

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -213,7 +213,7 @@ fun sin(x: Double, eps: Double): Double {
  */
 fun cos(x: Double, eps: Double): Double {
     var i = 0
-    var b = 1
+    var b = 1.0
     var cos = b
     while (b < eps){
         i += 2
