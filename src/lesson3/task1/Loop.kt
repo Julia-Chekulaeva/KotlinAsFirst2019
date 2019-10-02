90a2e82309a7141b5b0bb4af7441ac9a1b61084d
Юлия <julia4eck@gmail.com>
2019-10-03 02:29:37 +0300

    Third lesson all except the sin and cos tasks

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -309,9 +309,9 @@ fun squareSequenceDigit(n: Int): Int {
  * Использовать операции со строками в этой задаче запрещается.
  */
 fun fibSequenceDigit(n: Int): Int {
-    var length = 1 // Та же длина строки, но теперь прямо до нужного числа
+    var length = 0 // Та же длина строки, но теперь прямо до нужного числа
     var i = 0 // Та же длина числа
-    var ind = 1 // Номер числа
+    var ind = 0 // Номер числа
     var border = 1 // Та же "граница"
     while (length < n){
         i += 1
@@ -322,6 +322,6 @@ fun fibSequenceDigit(n: Int): Int {
         }
     }
     var num = fib(ind)
-    for (k in 1..(length - n)) num = num / 10
+    for (k in 1..(length - n + 1)) num = num / 10
     return num % 10
 }
