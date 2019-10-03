0e822c18174d210c80e05d54fb8ca3e8d153ca17
Юлия <julia4eck@gmail.com>
2019-10-03 16:08:30 +0300

    Third lesson all except the sin and cos tasks

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -320,7 +320,7 @@ fun fibSequenceDigit(n: Int): Int {
             ind += 1
             length += i
         }
-        if (fib(ind) >= border){
+        if (fib(ind) >= border && length < n){
             ind -= 1
             length -= i
         }
