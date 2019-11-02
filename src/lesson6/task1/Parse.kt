4bd0dd727147044ea4cf0a97b080d4fa72765327
Юлия <julia4eck@gmail.com>
2019-11-03 02:24:02 +0300

    All tasks except the last from 6 lesson

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -326,6 +326,7 @@ fun mostExpensive(description: String): String {
  *
  * Вернуть -1, если roman не является корректным римским числом
  */
+
 fun fromRoman(roman: String): Int {
     if (roman == "") return -1
     val digits = listOf(
