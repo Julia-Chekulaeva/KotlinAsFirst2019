9a28ebab89e982d4fe88afc7fa7fe50dfcc1f96d
Юлия <julia4eck@gmail.com>
2020-02-25 23:13:54 +0300

    Task_1

--- src/task_1/Polynominal.kt
+++ src/task_1/Polynominal.kt
@@ -1,25 +1,27 @@
 package task_1
 
+fun notEmptyList(list: List<Double>) = if (list.isEmpty()) listOf(0.0) else list
+
 data class Polynominal private constructor(val coefficients: List<Double>) {
 
     constructor(vararg coeffsWithZerous: Double) :
-            this(coeffsWithZerous.dropWhile { it < 10000 * Math.ulp(it) && it > -10000 * Math.ulp(it) })
+            this(notEmptyList(coeffsWithZerous.dropWhile { it < 10000 * Math.ulp(it) && it > -10000 * Math.ulp(it) }))
 
     val degree = coefficients.lastIndex
 
     private val reversedCoeffs = coefficients.reversed()
 
     operator fun plus(other: Polynominal): Polynominal {
-        val list = other.reversedCoeffs // Почему это работает, если свойство приватное...
+        val list = other.reversedCoeffs
         return if (degree > other.degree) Polynominal(*(list.withIndex().map { it.value + reversedCoeffs[it.index] }
                 + reversedCoeffs.subList(other.degree + 1, degree + 1)).reversed().toDoubleArray())
-        else Polynominal((reversedCoeffs.withIndex().map { it.value + list[it.index] }
-                + list.subList(degree + 1, other.degree + 1)).reversed())
+        else Polynominal(*(reversedCoeffs.withIndex().map { it.value + list[it.index] }
+                + list.subList(degree + 1, other.degree + 1)).reversed().toDoubleArray())
     }
 
-    operator fun minus(other: Polynominal): Polynominal = plus(Polynominal(other.coefficients.map { -it }))
+    operator fun minus(other: Polynominal) = plus(Polynominal(other.coefficients.map { -it }))
 
-    operator fun times(a: Double) = Polynominal(coefficients.map { it * a })
+    operator fun times(a: Double) = Polynominal(*coefficients.map { it * a }.toDoubleArray())
 
     operator fun times(other: Polynominal): Polynominal {
         val list = MutableList(degree + other.degree + 1) { 0.0 }
@@ -28,32 +30,32 @@ data class Polynominal private constructor(val coefficients: List<Double>) {
         for ((i, coeff1) in list1)
             for ((j, coeff2) in list2)
                 list[i + j] += coeff1 * coeff2
-        return Polynominal(list.reversed())
+        return Polynominal(*list.reversed().toDoubleArray())
     }
 
     private fun division(other: Polynominal): Pair<Polynominal, Polynominal> {
         val dividedPolynomCoeffs = coefficients.toMutableList()
-        val divisiorCoeff = other.coefficients[0]
+        val divisiorCoeffs = other.coefficients
+        val firstDivCoeff = divisiorCoeffs[0]
         val list = mutableListOf<Double>()
         for (i in degree - other.degree downTo 0) {
-            val a = dividedPolynomCoeffs[0] / divisiorCoeff
+            val a = dividedPolynomCoeffs[0] / firstDivCoeff
             list.add(a)
-            other.coefficients.withIndex().forEach { dividedPolynomCoeffs[it.index] -= it.value * a }
+            divisiorCoeffs.withIndex().forEach { dividedPolynomCoeffs[it.index] -= it.value * a }
             dividedPolynomCoeffs.removeAt(0)
         }
-        return Polynominal(list) to Polynominal(dividedPolynomCoeffs)
+        return Polynominal(*list.toDoubleArray()) to Polynominal(*dividedPolynomCoeffs.toDoubleArray())
     }
 
-    operator fun div(other: Polynominal): Polynominal = division(other).first
+    operator fun div(other: Polynominal) = division(other).first
 
-    operator fun rem(other: Polynominal): Polynominal = division(other).second
+    operator fun rem(other: Polynominal) = division(other).second
 
     fun valueOfExpression(x: Int) = coefficients.fold(0.0) { prev, coeff ->
         prev * x + coeff
     }
 
     override fun equals(other: Any?): Boolean {
-        //return super.equals(other)
         require(other is Polynominal)
         return coefficients == other.coefficients
     }
@@ -61,5 +63,11 @@ data class Polynominal private constructor(val coefficients: List<Double>) {
     override fun hashCode() = coefficients.hashCode()
 
     override fun toString() =
-        coefficients.withIndex().joinToString(" + ") { "${it.value} * x^${degree - it.index}" }.removeSuffix(" * x^0")
+        coefficients.subList(1, degree + 1).withIndex().joinToString(
+            "",
+            "${coefficients[0]} * x^$degree"
+        ) {
+            if (it.value >= 0.0) " + ${it.value} * x^${degree - 1 - it.index}"
+            else " - ${-it.value} * x^${degree - 1 - it.index}"
+        }.removeSuffix(" * x^0")
 }
--- test/task_1/PolynomTests.kt
+++ test/task_1/PolynomTests.kt
@@ -7,6 +7,16 @@ import org.junit.jupiter.api.Test
 class PolynomTests {
     @Test
     @Tag("Example")
+    fun constructor() {
+        assertEquals(listOf(1.0, 3.0, 0.0), Polynominal(0.0, 0.0, 1.0, 3.0, 0.0).coefficients)
+        assertEquals(listOf(1.0, 3.0, 0.0), Polynominal(1.0, 3.0, 0.0).coefficients)
+        assertEquals(listOf(0.0), Polynominal().coefficients)
+        assertEquals(listOf(0.0), Polynominal(0.0).coefficients)
+        assertEquals(listOf(0.0), Polynominal(0.0, 0.0).coefficients)
+    }
+
+    @Test
+    @Tag("Example")
     fun plus() {
         assertEquals(
             Polynominal(2.0, 1.0, 5.0),
@@ -32,7 +42,100 @@ class PolynomTests {
         assertEquals(
             Polynominal(1.0, 5.0),
             Polynominal(1.0, 3.0, 5.0) - Polynominal(1.0, 2.0, 0.0)
-        )// Почему тест ниже проходит, а этот - нет...
-        assertEquals(Polynominal(1.0, 3.0, 0.0), Polynominal(0.0, 0.0, 1.0, 3.0, 0.0))
+        )
+    }
+
+    @Test
+    @Tag("Example")
+    fun times() {
+        assertEquals(
+            Polynominal(1.0, -3.0, 3.0, -1.0),
+            Polynominal(1.0, -1.0) * Polynominal(1.0, -2.0, 1.0)
+        )
+    }
+
+    @Test
+    @Tag("Example")
+    fun div() {
+        assertEquals(
+            Polynominal(1.0, -1.0),
+            Polynominal(1.0, -3.0, 3.0, -1.0) / Polynominal(1.0, -2.0, 1.0)
+        )
+        assertEquals(
+            Polynominal(1.0, -2.0, 1.0),
+            Polynominal(1.0, -3.0, 3.0, -1.0) / Polynominal(1.0, -1.0)
+        )
+        assertEquals(
+            Polynominal(1.0, -2.0, 0.0),
+            Polynominal(1.0, -3.0, 2.0, 1.0) / Polynominal(1.0, -1.0)
+        )
+        assertEquals(
+            Polynominal(1.0, -1.0),
+            Polynominal(1.0, -3.0, 2.0, 1.0) / Polynominal(1.0, -2.0, 1.0)
+        )
+    }
+
+    @Test
+    @Tag("Example")
+    fun mod() {
+        assertEquals(
+            Polynominal(0.0),
+            Polynominal(1.0, -3.0, 3.0, -1.0) % Polynominal(1.0, -1.0)
+        )
+        assertEquals(
+            Polynominal(0.0),
+            Polynominal(1.0, -3.0, 3.0, -1.0) % Polynominal(1.0, -2.0, 1.0)
+        )
+        assertEquals(
+            Polynominal(1.0),
+            Polynominal(1.0, -3.0, 2.0, 1.0) % Polynominal(1.0, -1.0)
+        )
+        assertEquals(
+            Polynominal(-1.0, 2.0),
+            Polynominal(1.0, -3.0, 2.0, 1.0) % Polynominal(1.0, -2.0, 1.0)
+        )
+    }
+
+    @Test
+    @Tag("Example")
+    fun valueOfExpression() {
+        assertEquals(0.0, Polynominal(1.0, 3.0, 3.0, 1.0).valueOfExpression(-1))
+        assertEquals(1.0, Polynominal(1.0, 3.0, 1.0).valueOfExpression(0))
+        assertEquals(0.0, Polynominal(0.0).valueOfExpression(9))
+    }
+
+    @Test
+    @Tag("Example")
+    fun equals() {
+        assertEquals(
+            true,
+            Polynominal(8.0, 7.0, -1.0, 0.0) == Polynominal(8.0, 7.0, -1.0, 0.0)
+        )
+        assertEquals(
+            true,
+            Polynominal(1.0, 3.0, 0.0) == Polynominal(0.0, 0.0, 1.0, 3.0, 0.0)
+        )
+        assertEquals(
+            true,
+            Polynominal(0.0) == Polynominal()
+        )
+        assertEquals(
+            false,
+            Polynominal(1.0, 3.0) == Polynominal(1.0, 3.0, 0.0)
+        )
+    }
+
+    @Test
+    @Tag("Example")
+    fun toStringOverride() {
+        assertEquals("0.0", Polynominal().toString())
+        assertEquals(
+            "9.0 * x^3 + 1.0 * x^2 - 8.0 * x^1 + 3.0",
+            Polynominal(9.0, 1.0, -8.0, 3.0).toString()
+        )
+        assertEquals(
+            "-1.0 * x^2 - 8.0 * x^1 + 3.0",
+            Polynominal(-1.0, -8.0, 3.0).toString()
+        )
     }
 }
