426610abdbf5944c080726d8a0c022272520914f
Юлия <julia4eck@gmail.com>
2020-03-19 15:56:24 +0300

    Task2 pack-rle

--- test/task_2/TestsPackPLE.kt
+++ test/task_2/TestsPackPLE.kt
@@ -12,6 +12,9 @@ class TestsPackPLE {
         utilite("pack-rle -z input/task2_input/task2.txt")
         utilite("pack-rle -u -out input/task2_input/resOfTask2RLE.txt input/task2_input/task2RLE.txt")
         utilite("pack-rle -u input/task2_input/task2RLE.txt")
+        utilite("pack-rle -z -out input/task2_input/resOfSomeText.txt input/task2_input/SomeText.txt")
+        utilite("pack-rle -u input/task2_input/SomeTextRLE.txt")
+        utilite("pack-rle -u -out input/task2_input/SomeTextFromRLE.txt input/task2_input/resOfSomeText.txt")
         assertEquals(
             File("input/task2_input/task2RLE.rle").readText(), File("input/task2_input/resOfTask2RLE.txt").readText()
         )
@@ -23,6 +26,12 @@ class TestsPackPLE {
         )
         assertEquals(
             File("input/task2_input/task2RLE.txt").readText(), File("input/task2_input/resOfTask2.txt").readText()
+        )
+        assertEquals(
+            File("input/task2_input/SomeText.txt").readText(), File("input/task2_input/SomeTextRLE.rle").readText()
+        )
+        assertEquals(
+            File("input/task2_input/SomeText.txt").readText(), File("input/task2_input/SomeTextFromRLE.txt").readText()
         )
     }
 
